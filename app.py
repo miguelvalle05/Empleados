ea1a708b8e08678f38b6462f77377653fff4b3f8
Miguel Valle <miguelvallesin@gmail.com>
2022-09-01 17:04:32 -0500

    editar empleados

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+from crypt import methods
 from flask import Flask
-from flask import render_template, request
+from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
 from datetime import datetime
 
@@ -34,6 +35,47 @@ def index():
 
 
     return render_template('empleados/index.html', empleados=empleados)
+
+@app.route('/destroy/<int:id>')
+def destroy(id):
+
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute("DELETE FROM empleados WHERE id=%s",(id))
+    conn.commit()
+    return redirect('/')
+
+@app.route('/edit/<int:id>')
+def edit(id):
+
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute("SELECT * FROM empleados WHERE id=%s",(id))
+    empleados=cursor.fetchall()
+    print(empleados)
+    conn.commit()
+
+    return render_template('empleados/edit.html',empleados=empleados)
+
+
+@app.route('/update', methods=['POST'])
+def update():
+    _nombre=request.form['txtNombre']
+    _correo=request.form['txtCorreo']
+    _foto=request.files['txtFoto']
+
+    id=request.form['txtID']
+
+    sql="UPDATE empleados SET nombre=%s,correo=%s WHERE id=%s;"
+
+    datos=(_nombre,_correo,id)
+
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql,datos)
+    conn.commit()
+
+    return redirect('/')
 
 @app.route('/create')
 def create():
